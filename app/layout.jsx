ef0bc60013c3dd8f43c86f1f9475c9a7962db654
diopmorfall <morfall.papydiop99@gmail.com>
2023-11-11 23:11:01 +0100

    Project setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,3 +15,5 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+//* this page contains all our pages
---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image' //* this is a Next component
+//* most of our content will be driven from here
+
+export default function Home() {
+    return (
+        <main>
+            <h2>Dashboard</h2>
+        </main>
+    )
+}
