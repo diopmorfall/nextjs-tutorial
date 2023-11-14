489f7b158124c91da3ac42bff0d123901e452a81
diopmorfall <morfall.papydiop99@gmail.com>
2023-11-14 21:10:43 +0100

    Form fixed

--- app/tickets/create/CreateForm.jsx
+++ app/tickets/create/CreateForm.jsx
@@ -3,7 +3,7 @@
 //* a good practice is to make the small client component, and then integrate it into a bigger server one
 //? in this case, only the form needs to be interactive
 
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
 
 export default function CreateForm(){
@@ -24,8 +24,8 @@ export default function CreateForm(){
             priority,
             user_email: 'hero.garp@marines.op'
         }
-        //! needs json-server, test it locally
-        const res = await fetch('localhost:4000/tickets', {
+
+        const res = await fetch('http://localhost:4000/tickets', {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(ticket)
@@ -41,42 +41,42 @@ export default function CreateForm(){
 
     return(
         <main>
-            <form className="w-1/2">
-            <label>
-                <span>Title:</span>
-                <input
-                    required 
-                    type="text"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
-                />
-            </label>
-            <label>
-                <span>Body:</span>
-                <textarea
-                    required
-                    onChange={(e) => setBody(e.target.value)}
-                    value={body}
-                />
-            </label>
-            <label>
-                <span>Priority:</span>
-                <select 
-                    onChange={(e) => setPriority(e.target.value)}
-                    value={priority}
-                >
-                    <option value="low">Low Priority</option>
-                    <option value="medium">Medium Priority</option>
-                    <option value="high">High Priority</option>
-                </select>
-            </label>
-            <button 
-                    className="btn-primary" 
-                    disabled={isLoading}
-                >
-                {isLoading && <span>Adding...</span>}
-                {!isLoading && <span>Add Ticket</span>}
-            </button>
+            <form onSubmit={handleSubmit} className="w-1/2">
+                <label>
+                    <span>Title:</span>
+                    <input
+                        required 
+                        type="text"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>Body:</span>
+                    <textarea
+                        required
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
+                    />
+                </label>
+                <label>
+                    <span>Priority:</span>
+                    <select 
+                        onChange={(e) => setPriority(e.target.value)}
+                        value={priority}
+                    >
+                        <option value="low">Low Priority</option>
+                        <option value="medium">Medium Priority</option>
+                        <option value="high">High Priority</option>
+                    </select>
+                </label>
+                <button 
+                        className="btn-primary" 
+                        disabled={isLoading}
+                    >
+                    {isLoading && <span>Adding...</span>}
+                    {!isLoading && <span>Add Ticket</span>}
+                </button>
             </form>
         </main>
     )
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,19 +1,23 @@
 import { Suspense } from "react";
 import TicketList from "./TicketList";
 import Loading from "../loading";
+import Link from "next/link";
 
 export default function Tickets(){
     return(
         <main>
-           <nav>
-            <div>
-                <h2>Tickets</h2>
-                <p><small>Open tickets</small></p>
-            </div>
-           </nav>
-           <Suspense fallback={<Loading />}>
-           <    TicketList/>
-           </Suspense>
+            <nav>
+                <div>
+                    <h2>Tickets</h2>
+                    <p><small>Open tickets</small></p>
+                </div>
+            </nav>
+            <Suspense fallback={<Loading />}>
+                <TicketList/>
+            </Suspense>
+            <Link href="/tickets/create">
+                <button className="btn-primary">Create Ticket</button>
+            </Link>
         </main>
     )
 }
